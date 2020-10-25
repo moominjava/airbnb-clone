1bafc23be81ff8316f7e6cef9fc05a8d7e23d706
moominjava <moominjava@gmail.com>
2020-10-25 15:25:25 +0900

    Add *

--- rooms/models.py
+++ rooms/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
+from users import models as user_models
+
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    pass
 
 
 class Room(core_models.TimeStampedModel):
@@ -16,3 +35,11 @@ class Room(core_models.TimeStampedModel):
     beds = models.IntegerField()
     bedrooms = models.IntegerField()
     baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name
