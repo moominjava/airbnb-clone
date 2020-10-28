64965101b80427e99e62c7c2fe3b27246cdd2fa9
moominjava <moominjava@gmail.com>
2020-10-28 21:40:53 +0900

    Add *

--- rooms/models.py
+++ rooms/models.py
@@ -21,28 +21,44 @@ class RoomType(AbstractItem):
 
     """ RoomType Model Definition """
 
-    pass
+    class Meta:
+        verbose_name = "Room Type"
 
 
 class Amenity(AbstractItem):
 
     """ Amenity Model Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
 
     """ Facility Model Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Facilites"
 
 
 class HouseRule(AbstractItem):
 
     """ HouseRule Model Definition """
 
-    pass
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -61,12 +77,12 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
     room_type = models.ForeignKey(
-        RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity)
-    facility = models.ManyToManyField(Facility)
-    house_rulse = models.ManyToManyField(HouseRule)
+        "RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facility = models.ManyToManyField("Facility", blank=True)
+    house_rulse = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
